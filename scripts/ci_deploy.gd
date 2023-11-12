70bc91b9dc0ef94ee4cb8995de0d38a05cb0d3a9
AzrulTahir <acetahir@gmail.com>
2023-11-13 00:45:26 +0800

    Add export and deployment scripts, and update CI workflow.

---
+++ scripts/ci_deploy.gd
@@ -0,0 +1,5 @@
+# ci_deploy.gd
+
+func deploy_to_device():
+    # Add deployment logic here
+    print("Deployment to Android device.")
---
+++ scripts/ci_export.gd
@@ -0,0 +1,16 @@
+# ci_export.gd
+
+func export_to_android():
+    # Export the project for Android
+    var export_path = "export/android/"
+    var project_path = "."
+    var android_template = "res://path/to/android_export_presets.cfg"
+
+    var export_flags = ["-path", export_path, "-export-debug"]
+
+    # Run export process
+    var export_result = OS.execute("godot", export_flags)
+    if export_result == OK:
+        print("Export to Android successful.")
+    else:
+        print("Export to Android failed.")
