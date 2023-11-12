f28460f53199eb293894743033ed56bd2b036245
AzrulTahir <acetahir@gmail.com>
2023-11-13 00:20:24 +0800

    Add asset import script and update CI script.

---
+++ asset_import.gd
@@ -0,0 +1,9 @@
+# asset_import.gd
+
+func import_assets():
+    # Logic to import MP4 video files for scenes
+    # Adjust paths and filenames based on your project structure
+    preload("res://scenes/Scene1.mp4")
+    preload("res://scenes/Scene2.mp4")
+    preload("res://scenes/Scene3.mp4")
+    # Add more preloads for additional scenes as needed
--- ci.gd
+++ ci.gd
@@ -1,11 +1,8 @@
+# ci.gd
+
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+    print("CI Script is running.")
+    import_assets()  # Call the asset import function
+    # Add more logic as needed for other steps
